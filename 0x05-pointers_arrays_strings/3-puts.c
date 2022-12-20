f13333476ad8b17dbc812ec6bcb6f50289f5be8f
Berkty Tassaw <56670510+brkty-co@users.noreply.github.com>
2022-12-20 22:48:56 +0300

    Update 3-puts.c
    update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,17 +2,16 @@
 /**
  *_strlen - finds and outputs length of string.
  *@s: pointer to string.
- *
  *Return: length of a string.
  */
 int _strlen(char *s)
 {
-	int i;
-	i = 0;
-	while (*s != '\0')
-	{
-		i++;
-		s++;
-	}
-	return (i);
+int i;
+i = 0;
+while (*s != '\0')
+{
+i++;
+s++;
+}
+return (i);
 }
