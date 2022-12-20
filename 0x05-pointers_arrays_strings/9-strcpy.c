7a4483a1bcc82b2968289e67226dcb384a620c10
Berkty Tassaw <56670510+brkty-co@users.noreply.github.com>
2022-12-20 23:07:08 +0300

    Update 9-strcpy.c
    changes to 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,12 +8,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int j;
-
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[j] = src[j];
-	}
-	dest[j] = '\0';
-	return (dest);
+int j;
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[j] = src[j];
+}
+dest[j] = '\0';
+return (dest);
 }
