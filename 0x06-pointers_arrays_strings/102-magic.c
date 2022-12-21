c859ae85b74d0d4590ff1719a22c3f04ebe997f2
Berkty Tassaw <56670510+brkty-co@users.noreply.github.com>
2022-12-21 22:31:34 +0300

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ *_strncat - function that concatenates two strings. it will use
+ * at most n bytes from src. src does not need to be null terminated.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *@n: number of bytes to be concatenated.
+ *Return: pointer to destination string.
+ */
 int main(void)
 {
 int n;
@@ -7,6 +14,8 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
+  
+  
 /*
 * write your line of code here...
 * Remember:
