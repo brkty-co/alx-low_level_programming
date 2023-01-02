5eae686d6647122c1f84e616f12845edb268f04f
Berkty Tassaw <56670510+brkty-co@users.noreply.github.com>
2023-01-02 22:15:33 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,6 @@
                                                                                                                                    
 /**                                                                                                                                
  * File: main.h                                                                                                                    
- * Auth: sam tech (samuel birhanu)                                                                                                 
  * description: Header file containing prototypes for all functions                                                                
  *       used in the 0x06-pointers_arrays_strings directory.                                                                       
  */                                                                                                                                
