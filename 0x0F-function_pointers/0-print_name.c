5feb3794f5b4f020ca1cadb2b4ddb00f6874f603
Berkty Tassaw <56670510+brkty-co@users.noreply.github.com>
2023-01-20 12:42:18 +0300

    say my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 /**
  * print_name - prints the name
- * @name: name
+ * @name: name to print
  * @f: function
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
